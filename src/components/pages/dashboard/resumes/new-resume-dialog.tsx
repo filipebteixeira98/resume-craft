f25c326ed0522ca4cda74818cf7de435c12681c9
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-01-25 16:01:53 -0300

    chore(ui): add new resume dialog component

---
+++ src/components/pages/dashboard/resumes/new-resume-dialog.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { FormProvider, useForm } from 'react-hook-form'
+
+import { Button } from '@/components/ui/button'
+import { BaseDialogProps, Dialog } from '@/components/ui/dialog'
+import { InputField } from '@/components/ui/input/field'
+
+type FormData = {
+  title: string
+}
+
+export function DashboardNewResumeDialog(props: BaseDialogProps) {
+  const methods = useForm<FormData>()
+
+  function onSubmit(data: FormData) {
+    console.log(data)
+  }
+
+  return (
+    <Dialog
+      {...props}
+      title="Create new resume"
+      description="To begin, choose a title for you resume"
+      content={
+        <FormProvider {...methods}>
+          <form
+            className="flex flex-col"
+            onSubmit={methods.handleSubmit(onSubmit)}
+          >
+            <InputField label="Title" name="title" required />
+            <Button type="submit" className="w-max mt-6 ml-auto">
+              Create
+            </Button>
+          </form>
+        </FormProvider>
+      }
+    />
+  )
+}
--- src/components/pages/dashboard/resumes/resumes-list/index.tsx
+++ src/components/pages/dashboard/resumes/resumes-list/index.tsx
@@ -1,10 +1,13 @@
 import { DashboardAddResumeButton } from '../add-resume-button'
+import { DashboardNewResumeDialog } from '../new-resume-dialog'
 import { DashboardResumeCard } from '../resume-card'
 
 export function DashboardResumesList() {
   return (
     <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 auto-rows-max gap-4 lg:gap-5 flex-1">
-      <DashboardAddResumeButton />
+      <DashboardNewResumeDialog>
+        <DashboardAddResumeButton />
+      </DashboardNewResumeDialog>
       <DashboardResumeCard />
     </section>
   )
